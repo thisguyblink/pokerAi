a7238515c2635c529f84f15f6444b77cdfa020d2
Patrick Smith <patrickliamsmith@gmail.com>
2025-05-07 00:03:35 -0700

    Added values for emotions and weights based on image detection

--- main.py
+++ main.py
@@ -112,5 +112,13 @@ def aiDecision(cardNums, cardSuites, emotion, weight):
 
 def response(cardNums, cardSuites, emotion, weight, img):
     emotion, weight = read_image(img)
-    aiDecision(cardNums, cardSuites, emotion, weight)
+    emoVal = 0
+    if emotion in ["angry", "fear", "disgust", "sad"]:
+        emoVal = .5
+    if emotion == "happy":
+        emoVal = -.5
+    if emotion == "suprise":
+        weight /= 2
+    aiDecision(cardNums, cardSuites, emoVal, weight)
 
+  
